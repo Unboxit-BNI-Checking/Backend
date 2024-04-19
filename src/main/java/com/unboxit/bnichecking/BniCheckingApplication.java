fc8fe83ff09887399cb9ddfa5d7d128197f65dad
Jonathan Richard Sugandhi <jonathanrichard466@gmail.com>
2024-04-19 13:44:11 +0700

    init: spring boot application

---
+++ src/main/java/com/unboxit/bnichecking/BniCheckingApplication.java
@@ -0,0 +1,13 @@
+package com.unboxit.bnichecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BniCheckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BniCheckingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unboxit/bnichecking/BniCheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unboxit.bnichecking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BniCheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
