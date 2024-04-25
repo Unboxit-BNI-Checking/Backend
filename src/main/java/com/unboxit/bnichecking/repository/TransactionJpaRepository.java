97c81d0c07350ea581a4a7a41e0584ac4ed3e304
Jonathan Richard Sugandhi <jonathanrichard466@gmail.com>
2024-04-25 13:29:13 +0700

    fix: sort get transactions by account number

--- src/main/java/com/unboxit/bnichecking/repository/TransactionJpaRepository.java
+++ src/main/java/com/unboxit/bnichecking/repository/TransactionJpaRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface TransactionJpaRepository extends JpaRepository<Transaction, Long> {
 
-    @Query(value = "SELECT * FROM transactions WHERE account_number_source = :accountNumberSource", nativeQuery = true)
+    @Query(value = "SELECT * FROM transactions WHERE account_number_source = :accountNumberSource ORDER BY created_at DESC", nativeQuery = true)
     List<Transaction> findTransactionsByAccountNumberSource(String accountNumberSource);
 
     @Query(value = "SELECT * FROM transactions WHERE account_number_destination = :accountNumberDestination", nativeQuery = true)
