e4b21d5f2df172cb31cf20afe0fada61b571f76f
Jonathan Richard Sugandhi <jonathanrichard466@gmail.com>
2024-05-01 17:26:30 +0700

    fix: favourite query

--- src/main/java/com/unboxit/bnichecking/entity/db/FavouriteDB.java
+++ src/main/java/com/unboxit/bnichecking/entity/db/FavouriteDB.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public interface FavouriteDB {
 
-    long getFavouriteId();
+    Long getFavouriteId();
 
     String getName();
 
--- src/main/java/com/unboxit/bnichecking/repository/FavouriteJpaRepository.java
+++ src/main/java/com/unboxit/bnichecking/repository/FavouriteJpaRepository.java
@@ -11,25 +11,25 @@ public interface FavouriteJpaRepository extends JpaRepository<Favourite, Long> {
 
     @Query(value="""
         SELECT
-            f.favourite_id,
-            f.favourite_account_number,
-            f.name,
-            f.user_id,
-            f.created_at,
-            f.updated_at,
-            f.deleted_at
+            f.favourite_id AS favouriteId,
+            f.favourite_account_number AS favouriteAccountNumber,
+            f.name AS name,
+            f.user_id AS userId,
+            f.created_at AS createdAt,
+            f.updated_at AS updatedAt,
+            f.deleted_at AS deletedAt
         FROM favourites f""", nativeQuery = true)
     List<FavouriteDB> findAllFavourites();
 
     @Query(value="""
         SELECT
-            f.favourite_id,
-            f.favourite_account_number,
-            f.name,
-            f.user_id,
-            f.created_at,
-            f.updated_at,
-            f.deleted_at
+            f.favourite_id AS favouriteId,
+            f.favourite_account_number AS favouriteAccountNumber,
+            f.name AS name,
+            f.user_id AS userId,
+            f.created_at AS createdAt,
+            f.updated_at AS updatedAt,
+            f.deleted_at AS deletedAt
         FROM favourites f
         WHERE user_id=:userId""", nativeQuery = true)
     List<FavouriteDB> findByUserId(Long userId);
